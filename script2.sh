22d5ff864db91b1d22ccd04b08963eb1f7d4454a
AudaciousRaccoon <antton.domercq@laposte.net>
2023-11-16 10:30:14 +0100

    version fonctionnelle de base de script2

--- script2.sh
+++ script2.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+sed -E "s/^/<li>/g" $1.csv > tmp
+sed -E "s/$/<\/li>/g" tmp > tmp2
+
+
+
+
 echo "<h1>$1</h1>" > $1.html
-cat $1.csv >> $1.html
+echo "<ul>" >> $1.html
+cat tmp2 >> $1.html
+echo "</ul>" >> $1.html
+
+
