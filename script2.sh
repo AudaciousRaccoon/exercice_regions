8fbbd11a6359dc901ec5150774b7dbb4e0c73077
AudaciousRaccoon <antton.domercq@laposte.net>
2023-11-16 11:31:00 +0100

    on a rajoute le nom des regions pour script2, c'est fancy

--- script2.sh
+++ script2.sh
@@ -3,10 +3,10 @@
 sed -E "s/^/<li>/g" $1.csv > tmp
 sed -E "s/$/<\/li>/g" tmp > tmp2
 
+nom_region=$(grep $1 regions | cut -d: -f2)
 
 
-
-echo "<h1>$1</h1>" > $1.html
+echo "<h1>Region $nom_region  indicatif $1</h1>" > $1.html
 echo "<ul>" >> $1.html
 cat tmp2 >> $1.html
 echo "</ul>" >> $1.html
